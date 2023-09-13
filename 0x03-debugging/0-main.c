daf9768d05ccf347c4f465cf90454415ed4fe35f
Unarine <munyaiunarine17@gmail.com>
2023-09-13 15:26:39 -0700

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Test fuction for positive or negative
+ * Return: 0
+ */
+
+int main(void)
+void positive_or_negative(int i);
+{
+	int i;
+
+	i = 0
+	positive_or_negative(i);
+
+return (0);
+}
